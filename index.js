1f4632522019d24774f2167889e4ed70cb69ebc0
anuj upadhyay <anujupadhyay189z@gmail.com>
2026-09-08 11:29:33 +0530

    Add files

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-console.log("hello world");
-console.log("anuj upadhyay");
-console.log("3+6=", 3 + 6);
-console.log("this is my first javascript program");
-console.log("This is an error message");
+// console.log("hello world");
+// console.log("anuj upadhyay");
+// console.log("3+6=", 3 + 6);
+// console.log("this is my first javascript program");
+// console.log("This is an error message");
 
-const EventEmitter = require("events");
-const eventEmitter = new EventEmitter();
+// const EventEmitter = require("events");
+// const eventEmitter = new EventEmitter();
 
-eventEmitter.on("lunch", () => {
-  console.log("Welcome");
-});
+// eventEmitter.on("lunch", () => {
+//   console.log("Welcome");
+// });
 
-eventEmitter.emit("lunch");
-eventEmitter.emit("lunch");
+// eventEmitter.emit("lunch");
+// eventEmitter.emit("lunch");
+
+
+const os = require("os");
+console.log("Free Memory:", os.freemem());
+console.log("Total Memory:", os.totalmem());
+console.log("Plateform", os.plateform());
+console.log()
